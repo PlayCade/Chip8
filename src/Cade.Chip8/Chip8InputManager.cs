4c90ed9a49fd1b32990754cd59a70a089e50742c
Glenn Hevey <glenn.hevey@gmail.com>
2021-03-07 10:46:16 +1100

    Added internal input structure.

--- src/Cade.Chip8/Chip8InputManager.cs
+++ src/Cade.Chip8/Chip8InputManager.cs
@@ -7,11 +7,13 @@ namespace Cade.Chip8
 {
     public sealed class Chip8InputManager : ICadeInputManager
     {
+        private readonly byte[] _keys = new byte[16];
+        
         private static Lazy<List<Input>> _lazy = new Lazy<List<Input>>(() => new List<Input>
         {
             new Input
                 {
-                    Name = "Button 1",
+                    Name = "Button 0",
                     Active = false,
                     Number = 1,
                     Player = 1,
@@ -19,7 +21,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 2",
+                    Name = "Button 1",
                     Active = false,
                     Number = 2,
                     Player = 1,
@@ -27,7 +29,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 3",
+                    Name = "Button 2",
                     Active = false,
                     Number = 3,
                     Player = 1,
@@ -35,7 +37,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 4",
+                    Name = "Button 3",
                     Active = false,
                     Number = 4,
                     Player = 1,
@@ -43,7 +45,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 5",
+                    Name = "Button 4",
                     Active = false,
                     Number = 5,
                     Player = 1,
@@ -51,7 +53,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 6",
+                    Name = "Button 5",
                     Active = false,
                     Number = 6,
                     Player = 1,
@@ -59,7 +61,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 7",
+                    Name = "Button 6",
                     Active = false,
                     Number = 7,
                     Player = 1,
@@ -67,7 +69,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 8",
+                    Name = "Button 7",
                     Active = false,
                     Number = 8,
                     Player = 1,
@@ -75,7 +77,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button 9",
+                    Name = "Button 8",
                     Active = false,
                     Number = 9,
                     Player = 1,
@@ -83,7 +85,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button A",
+                    Name = "Button 9",
                     Active = false,
                     Number = 10,
                     Player = 1,
@@ -91,7 +93,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button B",
+                    Name = "Button A",
                     Active = false,
                     Number = 11,
                     Player = 1,
@@ -99,7 +101,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button C",
+                    Name = "Button B",
                     Active = false,
                     Number = 12,
                     Player = 1,
@@ -107,7 +109,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button D",
+                    Name = "Button C",
                     Active = false,
                     Number = 13,
                     Player = 1,
@@ -115,7 +117,7 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button E",
+                    Name = "Button D",
                     Active = false,
                     Number = 14,
                     Player = 1,
@@ -123,9 +125,17 @@ namespace Cade.Chip8
                 },
                 new Input
                 {
-                    Name = "Button F",
+                    Name = "Button E",
                     Active = false,
                     Number = 15,
+                    Player = 1,
+                    Type = InputType.Button
+                },
+                new Input
+                {
+                    Name = "Button F",
+                    Active = false,
+                    Number = 16,
                     Player = 1,
                     Type = InputType.Button
                 }
@@ -138,13 +148,73 @@ namespace Cade.Chip8
 
         public void Update(List<Input> inputs)
         {
-            
             _lazy = new Lazy<List<Input>>(() => inputs);
         }
 
         int ICadeInputManager.MaxPlayers()
         {
             return 1;
+        }
+
+        public byte[] CheckKeys()
+        {
+            var inputs = _lazy.Value;
+
+            foreach (var input in inputs)
+            {
+                switch (input.Number)
+                {
+                    case 1:
+                        _keys[0x0] = Convert.ToByte(input.Active);
+                        break;
+                    case 2:
+                        _keys[0x1] = Convert.ToByte(input.Active);
+                        break;
+                    case 3:
+                        _keys[0x2] = Convert.ToByte(input.Active);
+                        break;
+                    case 4:
+                        _keys[0x3] = Convert.ToByte(input.Active);
+                        break;
+                    case 5:
+                        _keys[0x4] = Convert.ToByte(input.Active);
+                        break;
+                    case 6:
+                        _keys[0x5] = Convert.ToByte(input.Active);
+                        break;
+                    case 7:
+                        _keys[0x6] = Convert.ToByte(input.Active);
+                        break;
+                    case 8:
+                        _keys[0x7] = Convert.ToByte(input.Active);
+                        break;
+                    case 9:
+                        _keys[0x8] = Convert.ToByte(input.Active);
+                        break;
+                    case 10:
+                        _keys[0x9] = Convert.ToByte(input.Active);
+                        break;
+                    case 11:
+                        _keys[0xA] = Convert.ToByte(input.Active);
+                        break;
+                    case 12:
+                        _keys[0xB] = Convert.ToByte(input.Active);
+                        break;
+                    case 13:
+                        _keys[0xC] = Convert.ToByte(input.Active);
+                        break;
+                    case 14:
+                        _keys[0xD] = Convert.ToByte(input.Active);
+                        break;
+                    case 15:
+                        _keys[0xE] = Convert.ToByte(input.Active);
+                        break;
+                    case 16:
+                        _keys[0xF] = Convert.ToByte(input.Active);
+                        break;
+                }
+            }
+            return _keys;
         }
     }
 }
